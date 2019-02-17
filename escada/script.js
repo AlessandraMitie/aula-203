7ab9071eb0fa42202cc54c71e5d4a1d822ca7079
alessandramitie <alessandra.mitie@gmail.com>
2019-02-17 14:03:01 -0300

    exercicio escada finalizado

--- escada/script.js
+++ escada/script.js
@@ -1,9 +1,9 @@
 let quantidadeDegrau = prompt("Digite o número de degraus que deseja");
 let materialDegrau = prompt("Digite qual o tipo de material voce quer");
 
-let escada;
+let escada = materialDegrau.toString();
 
-for (let i=1; i<= quantidadeDegrau; i++){
-    escada += materialDegrau;
+for (let i=1; i <= quantidadeDegrau; i++){
     console.log(escada);
+    escada += materialDegrau;
 }
